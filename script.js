fa43c1233c84cf1d1eb3f282fe874e14438f14c5
Komil-axiy2 <Komil7024868@gmail.com>
2024-08-11 18:40:24 +0500

    first-commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const weddingDate = new Date("October 20, 2029 12:00:00").getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const distance = weddingDate - now;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days + " <span>kun</span>";
+  document.getElementById("hours").innerHTML = hours + " <span>soat</span>";
+  document.getElementById("minutes").innerHTML = minutes + " <span>daqiqalar</span>";
+  document.getElementById("seconds").innerHTML = seconds + " <span>soniyalar</span>";
+
+  if (distance < 0) {
+    clearInterval(interval);
+    document.querySelector(".countdown").innerHTML = "To'y boshlandi!";
+  }
+}
+
+const interval = setInterval(updateCountdown, 1000);
